04f7c88ca80ac3ab0de3615e975d80f26b6b443c
damotr0n <44063700+damotr0n@users.noreply.github.com>
2023-03-07 10:52:58 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Artlogic Front-end Challenge
+
+## Viewing the project
+
+Run the following commands to view this project:
+- `npm install`
+- `npm start` 
+
+## Some notes
+
+- The background image is very large, it would be best to optimize it in some way for different device sizes.
+- I have decided to import the background image in CSS. This is not a good option for accessibility, however the background does not serve any functional purpose on this page. If the image was important for the functionality, I would import it as a HTML `img` element with the appropriate `alt` text.
+- Data fetching is abstracted away in a separate function, so that it can be interchanged or mocked easily in the main function.
+- This project has been built with Node v18, please keep this in mind when using older versions e.g. v12
+- I took ~2.5h to complete this fully.
---
+++ src/js/fetch-questions.js
@@ -0,0 +1,52 @@
+
+// --------------------------------------------------
+// data fetching
+
+const fetchQuestionData = () => {
+    return $.ajax({
+        url: "data.json", 
+        success: (res) => res
+    })
+}
+
+// --------------------------------------------------
+// helpers
+
+const onClick = (index) => {
+    $("#" + index + ".question").toggleClass("open")
+    $("#" + index + ".question .question-content").slideToggle(250);
+}
+
+const createQuestion = (title, content, index) => {
+    let questionItem = $("<li id='" + index + "' class='question'>")
+
+    let questionTitle = $("<div class='question-title'>")
+    questionTitle.append("<p>" + index + ". " + title + "</p>")
+    questionTitle.append("<div class='arrow-down' />")
+
+    questionItem.append(questionTitle)
+    questionItem.append("<p class='question-content'>" + content + "</p>")
+    questionItem.click(() => {
+        onClick(index);
+    })
+    return questionItem
+}
+
+const createQuestionList = (questionData) => {
+    return questionData.map((q, index) => {
+        return createQuestion(q.title, q.content, index + 1)
+    })
+}
+
+// --------------------------------------------------
+// main execution
+
+$("#question-list").ready(async () => {
+    const questionData = await fetchQuestionData()
+    const questions = createQuestionList(questionData.rows)
+    $("#question-list").append(questions)
+    // avoids popping from the above append
+    // would require a loading screen for any long API calls
+    $("body").removeClass("hidden")
+})
+
